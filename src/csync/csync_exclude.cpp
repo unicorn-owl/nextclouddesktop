64d09ddcb0acf60e1cb29b852cf1ead25e85d7ed
Camila <hello@camila.codes>
2020-10-02 08:57:15 +0200

    Add last / to exclude list file path.
    This Q_ASSERT(this->endsWith('/')) in BasePathByteArray was the reason
    for bug report #1458.
    
    Signed-off-by: Camila <hello@camila.codes>

--- src/csync/csync_exclude.cpp
+++ src/csync/csync_exclude.cpp
@@ -414,7 +414,9 @@ CSYNC_EXCLUDE_TYPE ExcludedFiles::traversalPatternMatch(const char *path, ItemTy
         QFileInfo fi = QFileInfo(_localPath + path + "/.sync-exclude.lst");
         if (fi.isReadable()) {
             addInTreeExcludeFilePath(fi.absoluteFilePath());
-            loadExcludeFile(fi.absolutePath().toUtf8(), fi.absoluteFilePath());
+
+            const auto basePath = leftIncludeLast(fi.absoluteFilePath().toUtf8(), '/');
+            loadExcludeFile(basePath, fi.absoluteFilePath());
         }
     }
 
