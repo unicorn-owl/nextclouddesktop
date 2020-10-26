AYCHNX/Desktop

Refactor for hardcoded strings

src/gui/folderstatusdelegate.cpp
@@ -20,6 +20,7 @@
 #include "accountstate.h"
 #include <theme.h>
 #include <account.h>
+#include <configfile.h>
 
 #include <QFileIconProvider>
 #include <QPainter>
@@ -153,9 +154,11 @@ void FolderStatusDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
     }
     painter->save();
 
+	ConfigFile cfgFile;
     QIcon statusIcon = qvariant_cast<QIcon>(index.data(FolderStatusIconRole));
     QString aliasText = qvariant_cast<QString>(index.data(HeaderRole));
-    QString pathText = qvariant_cast<QString>(index.data(FolderPathRole));
+    //QString pathText = qvariant_cast<QString>(index.data(FolderPathRole));
+	QString pathText = cfgFile.getFsSyncPath();
     QString remotePath = qvariant_cast<QString>(index.data(FolderSecondPathRole));
     QStringList conflictTexts = qvariant_cast<QStringList>(index.data(FolderConflictMsg));
     QStringList errorTexts = qvariant_cast<QStringList>(index.data(FolderErrorMsg));

unicorn-owl/nextclouddesktop

src/gui/folderstatusdelegate.cpp
@@ -21,6 +21,7 @@
 #include "accountstate.h"
 #include <theme.h>
 #include <account.h>
+#include <configfile.h>
 
 #include <QFileIconProvider>
 #include <QPainter>
@@ -156,9 +157,11 @@ void FolderStatusDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
     }
     painter->save();
 
+    ConfigFile cfgFile;
     auto statusIcon = qvariant_cast<QIcon>(index.data(FolderStatusIconRole));
     auto aliasText = qvariant_cast<QString>(index.data(HeaderRole));
-    auto pathText = qvariant_cast<QString>(index.data(FolderPathRole));
+    //auto pathText = qvariant_cast<QString>(index.data(FolderPathRole));
+    auto pathText = cfgFile.getFsSyncPath();
     auto remotePath = qvariant_cast<QString>(index.data(FolderSecondPathRole));
     auto conflictTexts = qvariant_cast<QStringList>(index.data(FolderConflictMsg));
     auto errorTexts = qvariant_cast<QStringList>(index.data(FolderErrorMsg));